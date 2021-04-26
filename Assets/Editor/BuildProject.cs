a32e0c810d049ba96f62e8f67b8ce85819d749b7
PockyGit <Adwin@live.cn>
2021-04-26 23:17:40 +0800

    test_build

---
+++ Assets/Editor/BuildProject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+
+public static class BuildProject
+{
+    public static void Build()
+    {
+        PlayerSettings.defaultIsFullScreen = false;
+        PlayerSettings.companyName = "pocky";
+        PlayerSettings.productName = "Texture_Normal";
+        PlayerSettings.resizableWindow = false;
+        PlayerSettings.displayResolutionDialog = ResolutionDialogSetting.Disabled;
+
+        string[] levels = { "Assets/Scenes/Texture_Normal.unity" };
+        BuildPipeline.BuildPlayer(levels, "test.apk", BuildTarget.Android, BuildOptions.None);
+    }
+}
